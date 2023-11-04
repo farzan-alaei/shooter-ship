d55036165ca934276af3fff591d7ad2644c37f9b
farzan-alaei <farzan.70@gmail.com>
2023-11-04 11:34:31 +0330

    update game_function.py and shooter-ship.py
    add function from main file(shooter_ship) to game_function

--- shooter-ship.py
+++ shooter-ship.py
@@ -1,7 +1,8 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
+
 
 def run_game():
     """ Initialize pygame, settings, and screen object """
@@ -16,15 +17,8 @@ def run_game():
 
     # start the main loop for the game.
     while True:
-        # watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            # Redraw the screen during each pass through the loop.
-            screen.fill(ai_settings.bg_color)
-            ship.blitme()
-            # make the most recently drawn screen visible.
-            pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
