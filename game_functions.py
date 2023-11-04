fa3db98ecac725a7b58d2c48ac14f25f84a08db2
farzan-alaei <farzan.70@gmail.com>
2023-11-04 13:06:05 +0330

    add bullet function to game_functions

--- game_functions.py
+++ game_functions.py
@@ -49,3 +49,14 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
     # make the most recently drawn screen visible.
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update Bullet positions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
