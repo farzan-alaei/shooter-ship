a8766c0e72fb87a196cfd867817f023b1f777d57
farzan-alaei <farzan.70@gmail.com>
2023-11-04 12:46:50 +0330

    add bullets to main file

--- bullet.py
+++ bullet.py
@@ -28,7 +28,7 @@ class Bullet(Sprite):
         self.y -= self.speed_factor
         # Update the rect position .
         self.rect.y = self.y
-        
+
     def draw_bullet(self):
         """Draw the bullet to the screen"""
         pygame.draw.rect(self.screen, self.color, self.rect)
--- shooter_ship.py
+++ shooter_ship.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -14,12 +15,15 @@ def run_game():
     pygame.display.set_caption("Shooter Ship")
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
