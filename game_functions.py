3c31a5d6ed437d854c04ab6ccf6ef7da06126583
farzan-alaei <farzan.70@gmail.com>
2023-11-05 10:10:24 +0330

    Adjusting the speed of the game after each time the aliens run out

--- game_functions.py
+++ game_functions.py
@@ -49,6 +49,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
     """Start a new game whet the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Reset the game settings.
+        ai_settings.initialize_dynamic_settings()
+
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
 
@@ -101,8 +104,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # Destroy existing bullets and create new fleet.
+        # Destroy existing bullets, speed up game, and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
